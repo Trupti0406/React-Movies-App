9f0eb817c40ac5517847f58d16036272be9a9841
trupti0406 <truptivijayyadav27@gmail.com>
2022-08-06 20:34:43 +0530

    homepage

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,46 +1,46 @@
-import React from 'react';
+import React from "react";
 import "./Header.css";
-import "./Banner.css"
+import "./Banner.css";
 
 function Home() {
   return (
     <>
-        <Header></Header>
-        <Banner></Banner>
-        <MovieList></MovieList>
-        <Pagination></Pagination>
-
+      <Header></Header>
+      <Banner></Banner>
+      <MovieList></MovieList>
+      <Pagination></Pagination>
     </>
-  )
+  );
 }
 
 export default Home;
 
 // Header
-function Header(){
-    return(
-        <div className='flex'>
-            <img src="https://img.icons8.com/external-bearicons-blue-bearicons/50/000000/external-movie-call-to-action-bearicons-blue-bearicons.png" />
-            <h2>Movies</h2>
-            <h2>Favourites</h2>
-        </div>
-    )
+function Header() {
+  return (
+    <div className="flex">
+      <img src="https://img.icons8.com/external-bearicons-blue-bearicons/50/000000/external-movie-call-to-action-bearicons-blue-bearicons.png" />
+      <h2>Movies</h2>
+      <h2>Favourites</h2>
+    </div>
+  );
 }
-
 
 // Banner
-function Banner(){
-    return(
-        <h2>Banner</h2>
-    )
+function Banner() {
+  // state variable
+  let [firstMovie, setMovie] = React.useState("");
+  React.useEffect(
+    function () {
+
+    }, []);
+  console.log("render");
+  return (
+    <>
+      <h1>I am Banner</h1>
+    </>
+  );
 }
-
-
-
-
-
-
-
 
 
 
@@ -56,16 +56,11 @@ function Banner(){
 
 
 // Movie List
-function MovieList(){
-    return(
-        <h2>Movie List</h2>
-    )
+function MovieList() {
+  return <h2>Movie List</h2>;
 }
 
-
-// Banner
-function Pagination(){
-    return(
-        <h2>Pagination</h2>
-    )
+// Pagination
+function Pagination() {
+  return <h2>Pagination</h2>;
 }
