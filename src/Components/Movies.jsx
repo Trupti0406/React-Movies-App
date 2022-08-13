1e1b93153c74dc1c4145e21f10bf679964b6c1ee
trupti0406 <truptivijayyadav27@gmail.com>
2022-08-14 01:30:50 +0530

    routing

--- src/Components/Movies.jsx
+++ src/Components/Movies.jsx
@@ -1,17 +1,31 @@
 // jsx gives you autocomplete feature
 // rfec
-import React from 'react'
-import Home from './Home'
-import Favourites from './Favourites'
+import React from "react";
+import Home from "./Home";
+import Favourites from "./Favourites";
+import PageNotFound from "./PageNotFound";
+import { Route, Redirect, Switch } from "react-router-dom";
 
 function movies() {
   return (
     <>
-        <Home></Home>
-        {/* <Favourites></Favourites> */}
+      <Switch>
+        <Route path="/home">
+          <Home></Home>
+        </Route>
+
+        <Route path="/favourites">
+          <Favourites></Favourites>
+        </Route>
+
+        <Redirect from="/" to="home" exact></Redirect>
+
+        <Route>
+          <PageNotFound></PageNotFound>
+        </Route>
+      </Switch>
     </>
-    
-  )
+  );
 }
 
 export default movies;
--- src/Components/PageNotFound.jsx
+++ src/Components/PageNotFound.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 function PageNotFound() {
-  return <h2>404--PageNotFound</h2>;
+  return <h2>Error: 404 PageNotFound</h2>;
 }
 
 export default PageNotFound;
